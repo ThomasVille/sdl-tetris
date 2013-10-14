8fceef4c7527c1e94f1e39ac07b4f6289b7c9a4e
WhiteMoll <c.thomas.quentin974@gmail.com>
2013-10-15 00:21:12 +0200

    Création des fonctions d'initialisation de la SDL et de la GameMatrix Ajout de la gestion des FPS

--- tetris.c
+++ tetris.c
@@ -10,7 +10,14 @@
 #include "gameLogic.h"
 
 int **createTable(int nbLin, int nbCol);
-int initSDL(SDL_Surface *screen);
+/// Initialise la SDL
+void initSDL(SDL_Surface **ptr_screen);
+/// Crée un GameMatrix
+GameMatrix* newGameMatrix(SDL_Surface *screen);
+/// Temporise (attend le temps nécessaire pour atteindre les FPS souhaités)
+void tempoFps(int *nextTick, int ticksBetweenFrames);
+/// Calcul les vrais FPS
+void computeTrueFps(int *realFps, int *realFpsCpt, int *lastSecondTicks);
 
 
 int main(int argc, char** argv)
@@ -19,7 +26,27 @@ int main(int argc, char** argv)
 	int continuer = 1, vitesse = 6, cadre = 10, score = 0, nbLignes = 0, nbLignesTotale = 0, scoring = 0;
 	int i = 0,j = 0;
 	clock_t depart, fin;
+	GameMatrix *surface = NULL;
+	
+	/// Titre de la fenêtre
+	char caption[64];
+	
+	/** Gérer les FPS **/
+	/// Nombre de FPS qu'on veut atteindre
+	int goalFps = 50;
+	/// Temps qui se passe entre deux frames (en millisecondes)
+	int ticksBetweenFrames = 1*(1000/goalFps);
+	/// Tick à attendre avant la prochaine frame (en millisecondes)
+	int nextTick = 0;
+	/// Vrais FPS
+	int realFps = goalFps, realFpsCpt = 0, lastSecondTicks = 0;
+	/// Compteur pour descendre la pièce mobile
+	int moveDownCpt = 0;
 
+	/** Variables SDL **/
+	SDL_Surface *screen = NULL, *texteScore = NULL;
+	SDL_Event event;
+	
 	// Tableau qui représente toutes les pièces possibles
 	int pieces[NB_PIECES][5][5] = {{{0,0,10,0,0},{0,0,10,0,0},{0,0,11,0,0},{0,0,10,0,0},{0,0,0,0,0}},
 								   {{0,0,0,0,0},{0,20,20,0,0},{0,20,21,0,0},{0,0,0,0,0},{0,0,0,0,0}},
@@ -29,66 +56,25 @@ int main(int argc, char** argv)
 								   {{0,0,0,0,0},{0,0,60,0,0},{0,60,61,0,0},{0,60,0,0,0},{0,0,0,0,0}},
 								   {{0,0,0,0,0},{0,0,70,0,0},{0,0,71,70,0},{0,0,0,70,0},{0,0,0,0,0}}};
 		
-	/** Variables SDL **/
-	SDL_Surface *screen = NULL, *texteScore = NULL;
-	SDL_Event event;
 	
-	/*************************** Initialisation SDL	***************************/ 
-	
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		fprintf(stderr, "Erreur à l'initialisation de la SDL : %s\n", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
- 
-	atexit(SDL_Quit);
-	screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_HWSURFACE || SDL_DOUBLEBUF);
- 
-	if (screen == NULL) {
-		fprintf(stderr, "Impossible d'activer le mode graphique : %s\n", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
- 
-	SDL_WM_SetCaption("Tetris Powaaaa !", NULL);
 	
 
-	/*************************** Initialisation SDL	***************************/ 
-/*	if(initSDL(screen) == EXIT_FAILURE)
-		return EXIT_FAILURE;
-	printf("%p\n", screen);*/
 	srand(time(NULL));
 
 
-	/** Crée la surface de jeu **/
-	GameMatrix *surface = malloc(sizeof(GameMatrix));
-	surface->height = 22;
-	surface->width = 10;
-	surface->surf = createTable(surface->height, surface->width);
-	// Calcul de la largeur d'un bloc en fonction des dimensions de la fenêtre
-	surface->coteBloc = (HEIGHT-CADRE*2)/surface->height;
-	
-	// Initialisation de la surface à 0
-	for(i = 0; i < surface->height; i++)
-		for(j = 0; j < surface->width; j++)
-			surface->surf[i][j] = 0;
-				
-			
-	surface->colors[0] = SDL_MapRGB(screen->format, 255,0,0); // Rouge
-	surface->colors[1] = SDL_MapRGB(screen->format, 0,0,255); // Bleu
-	surface->colors[2] = SDL_MapRGB(screen->format, 189,141,70); // Brun
-	surface->colors[3] = SDL_MapRGB(screen->format, 255,0,255); // Magenta
-	surface->colors[4] = SDL_MapRGB(screen->format, 255,255,255); // Blanc
-	surface->colors[5] = SDL_MapRGB(screen->format, 0,255,255); // Cyan
-	surface->colors[6] = SDL_MapRGB(screen->format, 0,255,0); // Vert
+	// Envoie l'adresse du pointeur de sorte à le modifier
+	initSDL(&screen);
+	// Crée une nouvelle surface de jeu
+	surface = newGameMatrix(screen);
 	
 		
 	addPiece(surface, pieces);
-	
-	depart = clock();
 		
 		
-	
 	do
 	{
+		moveDownCpt++;
+		realFpsCpt++;
 		nbLignes = 0;		
 		scoring = 0;
 		/* Gère les évenements */
@@ -145,10 +131,18 @@ int main(int argc, char** argv)
 		drawGameMatrix(screen, surface, cadre);
 		SDL_Flip(screen);
 		
-		if(clock() > depart + (CLOCKS_PER_SEC)/vitesse)
+		// Régule les FPS
+		tempoFps(&nextTick, ticksBetweenFrames);
+		// Calcul les vrais FPS
+		computeTrueFps(&realFps, &realFpsCpt, &lastSecondTicks);
+		
+		// Met à jour le titre de la fenêtre
+		sprintf(caption, "Tetris Powaaa ! %d fps !", realFps);
+		SDL_WM_SetCaption(caption, NULL);
+
+		if(moveDownCpt >= realFps/vitesse)
 		{
-			
-			depart = clock();
+			moveDownCpt = 0;
 			if(moveDown(surface) == 1)
 			{
 				
@@ -179,7 +173,6 @@ int main(int argc, char** argv)
 				
 			}
 		}
-		
 	}while (continuer);
 	
 	SDL_FreeSurface(screen);
@@ -200,10 +193,83 @@ int **createTable(int nbLin, int nbCol){
 	return tableau;
 }
 
-/*
-int initSDL(SDL_Surface *screen)
-{
-		
-	return 0;
-}
+/* Initialise la SDL
+ * ptr_screen : Pointeur sur le pointeur de la SDL_Surface
+ * On récupère un pointeur sur un pointeur pour modifier le pointeur
+ * original
 */
+void initSDL(SDL_Surface **ptr_screen)
+{
+	/*************************** Initialisation SDL	***************************/ 
+	atexit(SDL_Quit);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "Erreur à l'initialisation de la SDL : %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+ 
+	// Modifie la valeur du pointeur 'screen'
+	*ptr_screen = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_SWSURFACE || SDL_DOUBLEBUF);
+	
+	if (*ptr_screen == NULL) {
+		fprintf(stderr, "Impossible d'activer le mode graphique : %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("%p\n", *ptr_screen);
+	SDL_WM_SetCaption("Tetris Powaaaa !", NULL);
+	
+
+	/*************************** Initialisation SDL	***************************/ 
+}
+
+GameMatrix* newGameMatrix(SDL_Surface *screen)
+{
+	int i = 0,j = 0;
+	
+	/** Crée la surface de jeu **/
+	GameMatrix *surface = malloc(sizeof(GameMatrix));
+	/// Dimensions de la grille de jeu
+	surface->height = 22;
+	surface->width = 10;
+	surface->surf = createTable(surface->height, surface->width);
+	
+	// Calcul de la largeur d'un bloc en fonction des dimensions de la fenêtre
+	surface->coteBloc = (HEIGHT-CADRE*2)/surface->height;
+	
+	// Initialisation de la surface à 0
+	for(i = 0; i < surface->height; i++)
+		for(j = 0; j < surface->width; j++)
+			surface->surf[i][j] = 0;
+				
+	// Initialisation des couleurs		
+	surface->colors[0] = SDL_MapRGB(screen->format, 255,0,0); // Rouge
+	surface->colors[1] = SDL_MapRGB(screen->format, 0,0,255); // Bleu
+	surface->colors[2] = SDL_MapRGB(screen->format, 189,141,70); // Brun
+	surface->colors[3] = SDL_MapRGB(screen->format, 255,0,255); // Magenta
+	surface->colors[4] = SDL_MapRGB(screen->format, 255,255,255); // Blanc
+	surface->colors[5] = SDL_MapRGB(screen->format, 0,255,255); // Cyan
+	surface->colors[6] = SDL_MapRGB(screen->format, 0,255,0); // Vert
+	
+	return surface;
+}
+
+
+void tempoFps(int *nextTick, int ticksBetweenFrames)
+{
+	// Si on doit attendre avant la prochaine frame
+	if(*nextTick > SDL_GetTicks())
+		SDL_Delay(*nextTick - SDL_GetTicks());
+	// On calcule le tick où la prochaine frame va s'afficher
+	*nextTick = SDL_GetTicks() + ticksBetweenFrames;
+}
+
+void computeTrueFps(int *realFps, int *realFpsCpt, int *lastSecondTicks)
+{
+	if(SDL_GetTicks() >= *lastSecondTicks+1000)
+	{
+		*realFps = *realFpsCpt;
+		*realFpsCpt = 0;
+		*lastSecondTicks = SDL_GetTicks();
+	}
+}
+
